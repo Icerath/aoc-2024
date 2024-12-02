17e7de9e1bb70febe9ad7b21ac017b05e21a7119
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-02 10:23:58 +0200

    some gross optimization to part1

--- src/day1.rs
+++ src/day1.rs
@@ -9,21 +9,92 @@ pub const EXAMPLE_1: &str = "
 3   3";
 
 pub fn part1(input: &str) -> u32 {
-    let mut lhs_list = vec![];
-    let mut rhs_list = vec![];
-    for line in input.trim().lines() {
-        let (lhs, rhs) = line.split_once("   ").unwrap();
-        lhs_list.push(lhs.parse::<i32>().unwrap());
-        rhs_list.push(rhs.parse::<i32>().unwrap());
-    }
+    let input = input.trim();
+    let [mut lhs_list, mut rhs_list] = parse(input);
+
     lhs_list.sort_unstable();
     rhs_list.sort_unstable();
 
-    let mut total_diff = 0;
-    for (lhs, rhs) in lhs_list.into_iter().zip(rhs_list) {
-        total_diff += lhs.abs_diff(rhs)
+    lhs_list
+        .iter()
+        .zip(rhs_list)
+        .map(|(lhs, rhs)| lhs.abs_diff(rhs))
+        .sum()
+}
+
+pub fn parse(input: &str) -> [Vec<u32>; 2] {
+    let input = input.trim();
+    let input_bytes = input.as_bytes();
+    match input.len() {
+        PRECISE_RN_PATH_LEN => part1_rn_precise_path(input_bytes.try_into().unwrap()),
+        PRECISE_NL_PATH_LEN => part1_nl_precise_path(input_bytes.try_into().unwrap()),
+        _ => part1_parse_generic(input),
     }
-    total_diff
+}
+
+const PRECISE_RN_PATH_LEN: usize = 1000 * 15 - 2;
+fn part1_rn_precise_path(input: &[u8; PRECISE_RN_PATH_LEN]) -> [Vec<u32>; 2] {
+    let mut lhs_list = vec![];
+    let mut rhs_list = vec![];
+
+    for i in 0..1000 {
+        let i = i * 15;
+        let lhs = parse_int5(input[i..i + 5].try_into().unwrap());
+        let rhs = parse_int5(input[i + 8..i + 13].try_into().unwrap());
+        lhs_list.push(lhs);
+        rhs_list.push(rhs);
+    }
+    [lhs_list, rhs_list]
+}
+
+const PRECISE_NL_PATH_LEN: usize = 1000 * 14 - 1;
+fn part1_nl_precise_path(input: &[u8; PRECISE_NL_PATH_LEN]) -> [Vec<u32>; 2] {
+    let mut lhs_list = vec![];
+    let mut rhs_list = vec![];
+
+    for i in 0..1000 {
+        let i = i * 14;
+        let lhs = parse_int5(input[i..i + 5].try_into().unwrap());
+        let rhs = parse_int5(input[i + 8..i + 13].try_into().unwrap());
+        lhs_list.push(lhs);
+        rhs_list.push(rhs);
+    }
+    [lhs_list, rhs_list]
+}
+
+fn part1_parse_generic(input: &str) -> [Vec<u32>; 2] {
+    let mut lhs_list = vec![];
+    let mut rhs_list = vec![];
+
+    for line in input.lines() {
+        let [lhs, rhs] = parse_line(line);
+        lhs_list.push(lhs);
+        rhs_list.push(rhs);
+    }
+    [lhs_list, rhs_list]
+}
+
+pub fn parse_line(line: &str) -> [u32; 2] {
+    match line.len() {
+        13 => {
+            let lhs = parse_int5(line.as_bytes()[0..5].try_into().unwrap());
+            let rhs = parse_int5(line.as_bytes()[8..13].try_into().unwrap());
+            [lhs, rhs]
+        }
+        5 => [(line.as_bytes()[0]) as u32, (line.as_bytes()[4]) as u32],
+        _ => {
+            let (lhs, rhs) = line.split_once("   ").unwrap();
+            [lhs, rhs].map(|num| num.parse().unwrap())
+        }
+    }
+}
+
+fn parse_int5(bytes: &[u8; 5]) -> u32 {
+    bytes[0] as u32 * 10000
+        + bytes[1] as u32 * 1000
+        + bytes[2] as u32 * 100
+        + bytes[3] as u32 * 10
+        + bytes[4] as u32
 }
 
 #[test]
@@ -33,8 +104,8 @@ fn part1_example() {
 
 #[test]
 fn part1_input() {
-    let input = std::fs::read_to_string("input/day1_part1").unwrap();
-    assert_eq!(part1(&input), 2086478);
+    let input = include_str!("../input/day1_part1");
+    assert_eq!(part1(input), 2086478);
 }
 
 pub fn part2(input: &str) -> i32 {
@@ -60,6 +131,6 @@ fn part2_example() {
 
 #[test]
 fn part2_input() {
-    let input = std::fs::read_to_string("input/day1_part1").unwrap();
-    assert_eq!(part2(&input), 24941624);
+    let input = include_str!("../input/day1_part1");
+    assert_eq!(part2(input), 24941624);
 }
