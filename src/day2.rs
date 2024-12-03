8391c43a645aa118ab98e13c3f8a6fdc84c6676a
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-03 17:52:26 +0200

    optimize day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,29 +1,26 @@
+use tinyvec::ArrayVec;
+
 pub fn part1(input: &str) -> u32 {
-    let mut safe_reports = 0;
-    for report in input.trim().lines() {
-        safe_reports += 1;
-        let mut prev_level = None;
-        let mut increasing = None;
-        for level in report.split_whitespace() {
-            let level = level.parse::<u32>().unwrap();
-            if let Some(prev_level) = prev_level {
-                if !(1..=3).contains(&level.abs_diff(prev_level)) {
-                    safe_reports -= 1;
-                    break;
-                }
-                match increasing {
-                    None => increasing = Some(level.cmp(&prev_level)),
-                    Some(increasing) if increasing == level.cmp(&prev_level) => {}
-                    _ => {
-                        safe_reports -= 1;
-                        break;
-                    }
-                }
-            }
-            prev_level = Some(level);
-        }
+    let input = input.as_bytes();
+    input
+        .split(|c| *c == b'\n')
+        .map(|line| {
+            let record = line.split(|c| *c == b' ').map(parse_int);
+            (is_record_safe(record) == 0) as u32
+        })
+        .sum()
+}
+
+#[inline(always)]
+fn parse_int(bytes: &[u8]) -> u8 {
+    unsafe {
+        std::hint::assert_unchecked(bytes.len() <= 2);
     }
-    safe_reports
+    match bytes.len() {
+        1 => bytes[0] - b'0',
+        2 => (bytes[0] - b'0') * 10 + bytes[1] - b'0',
+        _ => unreachable!(),
+    }
 }
 
 #[test]
@@ -38,49 +35,60 @@ fn test_part1_input() {
     assert_eq!(part1(input), 686)
 }
 
-pub fn part2(input: &str) -> u32 {
-    let mut safe_reports = 0;
-
-    for report in input.trim().lines() {
-        let safe = part2_impl(report);
-        safe_reports += safe as u32;
-    }
-    safe_reports
+#[inline(always)]
+fn remove_level(record: &[u8], level: u8) -> impl Iterator<Item = u8> + '_ {
+    (0u8..).zip(record.iter().copied()).filter(move |&(i, _)| i != level).map(|(_, v)| v)
 }
 
-fn part2_impl(report: &str) -> bool {
-    let levels: Vec<u32> = report.split_whitespace().map(|v| v.parse().unwrap()).collect();
+pub fn part2(input: &str) -> u32 {
+    let input = input.as_bytes();
 
-    for try_skip in 0..=levels.len() {
-        let mut prev_level = None;
-        let mut safe = true;
-        let mut increasing = None;
+    input
+        .split(|c| *c == b'\n')
+        .map(|line| {
+            let mut record = ArrayVec::<[u8; 8]>::new();
+            record.truncate(0);
+            line.split(|c| *c == b' ').map(parse_int).for_each(|num| unsafe {
+                record.as_mut_ptr().add(record.len()).write(num);
+                record.set_len(record.len() + 1);
+            });
+            match is_record_safe(record.iter().copied()) {
+                0 => 1,
+                n @ 1.. => filter_record_safe(&record, n) as u32,
+            }
+        })
+        .sum()
+}
 
-        for (i, &level) in levels.iter().enumerate() {
-            if (i + 1) == try_skip {
-                continue;
-            }
-            if let Some(prev_level) = prev_level {
-                if !(1..=3).contains(&level.abs_diff(prev_level)) {
-                    safe = false;
-                    break;
-                }
-                match increasing {
-                    None => increasing = Some(level.cmp(&prev_level)),
-                    Some(increasing) if increasing == level.cmp(&prev_level) => {}
-                    _ => {
-                        safe = false;
-                        break;
-                    }
-                }
-            }
-            prev_level = Some(level);
-        }
-        if safe {
-            return true;
-        }
+fn filter_record_safe(numbers: &[u8], n: u8) -> bool {
+    if is_record_safe(remove_level(numbers, n)) == 0 {
+        return true;
+    }
+    if is_record_safe(remove_level(numbers, n - 1)) == 0 {
+        return true;
+    }
+    if is_record_safe(remove_level(numbers, n + 1)) == 0 {
+        return true;
     }
     false
+}
+
+fn is_record_safe(mut record: impl Iterator<Item = u8>) -> u8 {
+    let current: u8 = unsafe { record.next().unwrap_unchecked() };
+    let next: u8 = unsafe { record.next().unwrap_unchecked() };
+    if !matches!(current.abs_diff(next), 1..=3) {
+        return 1;
+    }
+    let direction = current < next;
+
+    let mut current = next;
+    for (index, next) in (0u8..).zip(record) {
+        if !(matches!(current.abs_diff(next), 1..=3) && (current < next) == direction) {
+            return index + 1;
+        }
+        current = next;
+    }
+    0
 }
 
 #[test]
