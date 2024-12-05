ce9618eab44566498325368085ffe9cf12b295b9
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-05 21:23:20 +0200

    day5: added benches

---
+++ benches/day5.rs
@@ -0,0 +1,15 @@
+use aoc_2024::day5::{part1, part2};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn bench_part1(c: &mut Criterion) {
+    let input = include_str!("../input/day5_part1");
+    c.bench_function("day5_part1", |b| b.iter(|| part1(black_box(input))));
+}
+
+fn bench_part2(c: &mut Criterion) {
+    let input = include_str!("../input/day5_part1");
+    c.bench_function("day5_part2", |b| b.iter(|| part2(black_box(input))));
+}
+
+criterion_group!(benches, bench_part1, bench_part2);
+criterion_main!(benches);
