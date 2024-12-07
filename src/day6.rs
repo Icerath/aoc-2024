f63ebb5a40e2018ddc20c18eadaddf6b0721b5c4
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-07 18:33:38 +0200

    day6: move 'go' functions to end

--- src/day6.rs
+++ src/day6.rs
@@ -55,6 +55,71 @@ fn test_part1_input() {
     assert_eq!(part1(include_str!("../input/day6_part1")), 5453);
 }
 
+#[expect(clippy::cast_possible_wrap, clippy::missing_panics_doc, clippy::cast_sign_loss)]
+pub fn part2(input: &str) -> u32 {
+    let input = input.as_bytes();
+    let initial_guard_position = input.find_byte(b'^').unwrap();
+
+    let mut squares_traversed = HashSet::new();
+
+    {
+        let mut guard_position = initial_guard_position;
+        let mut direction = UP;
+
+        loop {
+            let forward_position = guard_position as isize + direction;
+            if forward_position < 0 || forward_position > input.len() as isize {
+                break;
+            }
+            let forward = input[forward_position as usize];
+            if forward == b'#' {
+                direction = match direction {
+                    UP => RIGHT,
+                    RIGHT => DOWN,
+                    DOWN => LEFT,
+                    LEFT => UP,
+                    _ => unsafe { unreachable_unchecked() },
+                };
+            } else if forward == b'\n' {
+                break;
+            } else {
+                guard_position = guard_position.wrapping_add_signed(direction);
+                squares_traversed.insert(guard_position);
+            }
+        }
+    }
+
+    let mut loop_obstacles = 0;
+    for object_square in squares_traversed {
+        if object_square == initial_guard_position {
+            continue;
+        }
+        let mut guard_position = initial_guard_position;
+        let mut history = [0; 131 * 130];
+        if unsafe { go_up(input, object_square, &mut history, &mut guard_position) } {
+            loop_obstacles += 1;
+        }
+    }
+
+    loop_obstacles
+}
+
+#[test]
+#[ignore]
+fn test_part2_example() {
+    assert_eq!(part2(include_str!("../input/day6_part1_example")), 6);
+}
+
+#[test]
+fn test_part2_input() {
+    assert_eq!(part2(include_str!("../input/day6_part1")), 2188);
+}
+
+const UP: isize = -131;
+const RIGHT: isize = 1;
+const DOWN: isize = 131;
+const LEFT: isize = -1;
+
 unsafe fn go_up(
     input: &[u8],
     object_square: usize,
@@ -121,7 +186,6 @@ unsafe fn go_right(
     history[*guard_position] |= 2;
     go_down(input, object_square, history, guard_position)
 }
-
 unsafe fn go_down(
     input: &[u8],
     object_square: usize,
@@ -168,68 +232,3 @@ unsafe fn go_left(
     }
     go_up(input, object_square, history, guard_position)
 }
-
-#[expect(clippy::cast_possible_wrap, clippy::missing_panics_doc, clippy::cast_sign_loss)]
-pub fn part2(input: &str) -> u32 {
-    let input = input.as_bytes();
-    let initial_guard_position = input.find_byte(b'^').unwrap();
-
-    let mut squares_traversed = HashSet::new();
-
-    {
-        let mut guard_position = initial_guard_position;
-        let mut direction = UP;
-
-        loop {
-            let forward_position = guard_position as isize + direction;
-            if forward_position < 0 || forward_position > input.len() as isize {
-                break;
-            }
-            let forward = input[forward_position as usize];
-            if forward == b'#' {
-                direction = match direction {
-                    UP => RIGHT,
-                    RIGHT => DOWN,
-                    DOWN => LEFT,
-                    LEFT => UP,
-                    _ => unsafe { unreachable_unchecked() },
-                };
-            } else if forward == b'\n' {
-                break;
-            } else {
-                guard_position = guard_position.wrapping_add_signed(direction);
-                squares_traversed.insert(guard_position);
-            }
-        }
-    }
-
-    let mut loop_obstacles = 0;
-    for object_square in squares_traversed {
-        if object_square == initial_guard_position {
-            continue;
-        }
-        let mut guard_position = initial_guard_position;
-        let mut history = [0; 131 * 130];
-        if unsafe { go_up(input, object_square, &mut history, &mut guard_position) } {
-            loop_obstacles += 1;
-        }
-    }
-
-    loop_obstacles
-}
-
-#[test]
-#[ignore]
-fn test_part2_example() {
-    assert_eq!(part2(include_str!("../input/day6_part1_example")), 6);
-}
-
-#[test]
-fn test_part2_input() {
-    assert_eq!(part2(include_str!("../input/day6_part1")), 2188);
-}
-
-const UP: isize = -131;
-const RIGHT: isize = 1;
-const DOWN: isize = 131;
-const LEFT: isize = -1;
