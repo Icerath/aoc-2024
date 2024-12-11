dff7061d262343e3d992dfa8999058fd7a6ae2f1
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-11 12:47:27 +0200

    assume input size

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,4 @@
 pub fn part1(input: &str) -> u32 {
-    let line_len = input.lines().next().unwrap().len() + 1;
     let input = input.as_bytes();
     let mut sum = 0;
     for (i, &b) in input.iter().enumerate() {
@@ -7,18 +6,12 @@ pub fn part1(input: &str) -> u32 {
             continue;
         }
         let mut places_visted = [false; 46 * 45];
-        sum += trail_count_from(input, line_len, i, 0, &mut places_visted);
+        sum += trail_count_from(input, i, 0, &mut places_visted);
     }
     sum
 }
 
-fn trail_count_from(
-    input: &[u8],
-    line_len: usize,
-    position: usize,
-    mut current: u8,
-    places_visted: &mut [bool],
-) -> u32 {
+fn trail_count_from(input: &[u8], position: usize, mut current: u8, places_visted: &mut [bool]) -> u32 {
     if places_visted[position] || input[position].wrapping_sub(b'0') != current {
         return 0;
     }
@@ -28,20 +21,21 @@ fn trail_count_from(
     }
     current += 1;
     let mut sum = 0;
-    sum += trail_count_from(input, line_len, position + 1, current, places_visted);
+    sum += trail_count_from(input, position + 1, current, places_visted);
     if position != 0 {
-        sum += trail_count_from(input, line_len, position - 1, current, places_visted);
+        sum += trail_count_from(input, position - 1, current, places_visted);
     }
-    if position >= line_len {
-        sum += trail_count_from(input, line_len, position - line_len, current, places_visted);
+    if position >= LINE_LEN {
+        sum += trail_count_from(input, position - LINE_LEN, current, places_visted);
     }
-    if position < (input.len() - line_len) {
-        sum += trail_count_from(input, line_len, position + line_len, current, places_visted);
+    if position < (input.len() - LINE_LEN) {
+        sum += trail_count_from(input, position + LINE_LEN, current, places_visted);
     }
     sum
 }
 
 #[test]
+#[ignore]
 fn test_part1_example() {
     assert_eq!(part1(include_str!("../input/day10_part1_example")), 36);
 }
@@ -52,19 +46,18 @@ fn test_part1_input() {
 }
 
 pub fn part2(input: &str) -> u32 {
-    let line_len = input.lines().next().unwrap().len() + 1;
     let input = input.as_bytes();
     let mut sum = 0;
     for (i, &b) in input.iter().enumerate() {
         if b != b'0' {
             continue;
         }
-        sum += part2_trail_count_from(input, line_len, i, 0);
+        sum += part2_trail_count_from(input, i, 0);
     }
     sum
 }
 
-fn part2_trail_count_from(input: &[u8], line_len: usize, position: usize, mut current: u8) -> u32 {
+fn part2_trail_count_from(input: &[u8], position: usize, mut current: u8) -> u32 {
     if input[position].wrapping_sub(b'0') != current {
         return 0;
     }
@@ -73,20 +66,21 @@ fn part2_trail_count_from(input: &[u8], line_len: usize, position: usize, mut cu
     }
     current += 1;
     let mut sum = 0;
-    sum += part2_trail_count_from(input, line_len, position + 1, current);
+    sum += part2_trail_count_from(input, position + 1, current);
     if position != 0 {
-        sum += part2_trail_count_from(input, line_len, position - 1, current);
+        sum += part2_trail_count_from(input, position - 1, current);
     }
-    if position >= line_len {
-        sum += part2_trail_count_from(input, line_len, position - line_len, current);
+    if position >= LINE_LEN {
+        sum += part2_trail_count_from(input, position - LINE_LEN, current);
     }
-    if position < (input.len() - line_len) {
-        sum += part2_trail_count_from(input, line_len, position + line_len, current);
+    if position < (input.len() - LINE_LEN) {
+        sum += part2_trail_count_from(input, position + LINE_LEN, current);
     }
     sum
 }
 
 #[test]
+#[ignore]
 fn test_part2_example() {
     assert_eq!(part2(include_str!("../input/day10_part1_example")), 81);
 }
@@ -95,3 +89,5 @@ fn test_part2_example() {
 fn test_part2_input() {
     assert_eq!(part2(include_str!("../input/day10_part1")), 1225);
 }
+
+const LINE_LEN: usize = 46;
