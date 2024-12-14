35ab59b7911c70ea320c618c8783a4b000648f00
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-14 15:30:44 +0200

    ?

--- src/day13.rs
+++ src/day13.rs
@@ -1,14 +1,14 @@
 unsafe fn both_parts<const OFFSET: i64>(input: &[u8]) -> i64 {
-    let mut remaining = input;
+    let mut remaining = input.as_ptr();
     let mut result = 0;
-    loop {
-        let x1 = parse2(remaining.get_unchecked(12..14));
-        let y1 = parse2(remaining.get_unchecked(18..20));
+    for _ in 0..320 {
+        let x1 = parse2(remaining.add(12));
+        let y1 = parse2(remaining.add(18));
 
-        let x2 = parse2(remaining.get_unchecked(12 + 21..14 + 21));
-        let y2 = parse2(remaining.get_unchecked(18 + 21..20 + 21));
+        let x2 = parse2(remaining.add(12 + 21));
+        let y2 = parse2(remaining.add(18 + 21));
 
-        remaining = remaining.get_unchecked(9 + 42..);
+        remaining = remaining.add(9 + 42);
         let [z1, z2] = parse_pair(&mut remaining);
         let [z1, z2] = [z1 + OFFSET, z2 + OFFSET];
 
@@ -16,10 +16,8 @@ unsafe fn both_parts<const OFFSET: i64>(input: &[u8]) -> i64 {
         let a = (z1 - b * x2).checked_div(x1).unwrap_unchecked();
         let res = a * 3 + b;
         result += res * ((x1 * a + x2 * b, y1 * a + y2 * b) == (z1, z2)) as i64;
-        if remaining.len() < 2 {
-            break;
-        }
-        remaining = remaining.get_unchecked(2..);
+        // using ptr::add here would be UB
+        remaining = remaining.wrapping_add(2);
     }
     result
 }
@@ -43,27 +41,27 @@ fn test_part2() {
 }
 
 #[inline(always)]
-unsafe fn parse2(bytes: &[u8]) -> i64 {
-    (*bytes.get_unchecked(0) as i64 * 10 + *bytes.get_unchecked(1) as i64) - (b'0' as i64 * 11)
+unsafe fn parse2(bytes: *const u8) -> i64 {
+    (bytes.read() as i64 * 10 + bytes.add(1).read() as i64) - (b'0' as i64 * 11)
 }
 
 #[inline(always)]
-unsafe fn parse_pair(ptr: &mut &[u8]) -> [i64; 2] {
-    let mut lhs = parse2(ptr);
-    *ptr = ptr.get_unchecked(2..);
+unsafe fn parse_pair(ptr: &mut *const u8) -> [i64; 2] {
+    let mut lhs = parse2(*ptr);
+    *ptr = ptr.add(2);
 
-    while *ptr.get_unchecked(0) != b',' {
-        lhs = lhs * 10 + (*ptr.get_unchecked(0) - b'0') as i64;
-        *ptr = ptr.get_unchecked(1..);
+    while ptr.read() != b',' {
+        lhs = lhs * 10 + (ptr.read() - b'0') as i64;
+        *ptr = ptr.add(1);
     }
-    *ptr = ptr.get_unchecked(4..);
+    *ptr = ptr.add(4);
 
-    let mut rhs = parse2(ptr);
-    *ptr = ptr.get_unchecked(2..);
+    let mut rhs = parse2(*ptr);
+    *ptr = ptr.add(2);
 
-    while *ptr.get_unchecked(0) != b'\n' {
-        rhs = rhs * 10 + (*ptr.get_unchecked(0) - b'0') as i64;
-        *ptr = ptr.get_unchecked(1..);
+    while ptr.read() != b'\n' {
+        rhs = rhs * 10 + (ptr.read() - b'0') as i64;
+        *ptr = ptr.add(1);
     }
     [lhs, rhs]
 }
