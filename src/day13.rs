50372ba555e1cd1fd0988e414a327b614f35bc82
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-14 15:50:58 +0200

    .

--- src/day13.rs
+++ src/day13.rs
@@ -1,18 +1,44 @@
 macro_rules! impl_part {
     ($input: ident, $offset: literal, $ty: ty) => {{
+        #[inline(always)]
+        unsafe fn parse2(bytes: *const u8) -> $ty {
+            (bytes.read() as $ty * 10 + bytes.add(1).read() as $ty) - (b'0' as $ty * 11)
+        }
+
+        #[inline(always)]
+        unsafe fn parse_pair(ptr: &mut *const u8) -> [$ty; 2] {
+            let mut lhs = parse2(*ptr);
+            *ptr = ptr.add(2);
+
+            while ptr.read() != b',' {
+                lhs = lhs * 10 + (ptr.read() - b'0') as $ty;
+                *ptr = ptr.add(1);
+            }
+            *ptr = ptr.add(4);
+
+            let mut rhs = parse2(*ptr);
+            *ptr = ptr.add(2);
+
+            while ptr.read() != b'\n' {
+                rhs = rhs * 10 + (ptr.read() - b'0') as $ty;
+                *ptr = ptr.add(1);
+            }
+            [lhs, rhs]
+        }
+
         let mut remaining = $input.as_ptr();
         let end = $input.as_ptr().add($input.len());
         let mut result = 0;
         while (remaining as usize) < end as usize {
-            let x1 = parse2(remaining.add(12)) as $ty;
-            let y1 = parse2(remaining.add(18)) as $ty;
+            let x1 = parse2(remaining.add(12));
+            let y1 = parse2(remaining.add(18));
 
-            let x2 = parse2(remaining.add(12 + 21)) as $ty;
-            let y2 = parse2(remaining.add(18 + 21)) as $ty;
+            let x2 = parse2(remaining.add(12 + 21));
+            let y2 = parse2(remaining.add(18 + 21));
 
             remaining = remaining.add(9 + 42);
             let [z1, z2] = parse_pair(&mut remaining);
-            let [z1, z2] = [z1 as $ty + $offset, z2 as $ty + $offset];
+            let [z1, z2] = [z1 + $offset, z2 + $offset];
 
             let b = (z2 * x1 - z1 * y1).checked_div(y2 * x1 - x2 * y1).unwrap_unchecked();
             let a = (z1 - b * x2).checked_div(x1).unwrap_unchecked();
@@ -41,30 +67,4 @@ fn test_part1() {
 #[test]
 fn test_part2() {
     assert_eq!(part2(include_str!("../input/day13.txt")), 92_871_736_253_789);
-}
-
-#[inline(always)]
-unsafe fn parse2(bytes: *const u8) -> i32 {
-    (bytes.read() as i32 * 10 + bytes.add(1).read() as i32) - (b'0' as i32 * 11)
-}
-
-#[inline(always)]
-unsafe fn parse_pair(ptr: &mut *const u8) -> [i32; 2] {
-    let mut lhs = parse2(*ptr);
-    *ptr = ptr.add(2);
-
-    while ptr.read() != b',' {
-        lhs = lhs * 10 + (ptr.read() - b'0') as i32;
-        *ptr = ptr.add(1);
-    }
-    *ptr = ptr.add(4);
-
-    let mut rhs = parse2(*ptr);
-    *ptr = ptr.add(2);
-
-    while ptr.read() != b'\n' {
-        rhs = rhs * 10 + (ptr.read() - b'0') as i32;
-        *ptr = ptr.add(1);
-    }
-    [lhs, rhs]
 }
