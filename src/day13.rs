510274b15271a43655cf8f3ad6f13fae7f261c8b
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-14 15:36:29 +0200

    ?

--- src/day13.rs
+++ src/day13.rs
@@ -1,7 +1,8 @@
 unsafe fn both_parts<const OFFSET: i64>(input: &[u8]) -> i64 {
     let mut remaining = input.as_ptr();
+    let end = input.as_ptr() as usize + input.len();
     let mut result = 0;
-    for _ in 0..320 {
+    while (remaining as usize) < end {
         let x1 = parse2(remaining.add(12));
         let y1 = parse2(remaining.add(18));
 
