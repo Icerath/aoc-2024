d92e38098a1f6e829caed2b3cc93cad0389b1902
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-15 13:25:14 +0200

    14: initial part2

--- benches/bench_today.rs
+++ benches/bench_today.rs
@@ -1,15 +1,15 @@
-use aoc_2024::day13::{part1, part2};
+use aoc_2024::day14::{part1, part2};
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
 
 fn bench_part1(c: &mut Criterion) {
-    let input = include_str!("../input/day13.txt");
-    c.bench_function("day13_part1", |b| b.iter(|| part1(black_box(input))));
+    let input = include_str!("../input/day14.txt");
+    c.bench_function("day14_part1", |b| b.iter(|| part1(black_box(input))));
 }
 
 fn bench_part2(c: &mut Criterion) {
-    let input = include_str!("../input/day13.txt");
-    c.bench_function("day13_part2", |b| b.iter(|| part2(black_box(input))));
+    let input = include_str!("../input/day14.txt");
+    c.bench_function("day14_part2", |b| b.iter(|| part2(black_box(input))));
 }
 
 criterion_group!(benches, bench_part1, bench_part2,);
--- src/day14.rs
+++ src/day14.rs
@@ -1,3 +1,4 @@
+#![expect(clippy::cast_sign_loss)]
 use bstr::ByteSlice;
 
 pub fn part1(input: &str) -> u32 {
@@ -36,7 +37,51 @@ pub fn part1(input: &str) -> u32 {
     quadrants.into_iter().product()
 }
 
+pub fn part2(input: &str) -> i32 {
+    let mut remaining = input.as_bytes();
+    let mut robots = vec![];
+    loop {
+        if remaining.is_empty() {
+            break;
+        }
+        remaining = &remaining[2..];
+        let comma = remaining.find_byte(b',').unwrap();
+        let px: i32 = remaining[..comma].to_str().unwrap().parse().unwrap();
+        remaining = &remaining[comma + 1..];
+        let space = remaining.find_byte(b' ').unwrap();
+        let py: i32 = remaining[..space].to_str().unwrap().parse().unwrap();
+        remaining = &remaining[space + 3..];
+        let comma = remaining.find_byte(b',').unwrap();
+        let vx: i32 = remaining[..comma].to_str().unwrap().parse().unwrap();
+        remaining = &remaining[comma + 1..];
+        let nl = remaining.find_byte(b'\n').unwrap();
+        let vy: i32 = remaining[..nl].to_str().unwrap().parse().unwrap();
+        remaining = &remaining[nl + 1..];
+
+        robots.push([px, py, vx, vy]);
+    }
+    for i in 0..(101 * 103) {
+        // FIXME: u128s are slow. Use something else.
+        let mut grid = [0u128; 103];
+        for [px, py, vx, vy] in &mut robots {
+            grid[*py as usize] |= 1u128 << *px;
+            *px = (*px + *vx).rem_euclid(101);
+            *py = (*py + *vy).rem_euclid(103);
+        }
+        // FIXME: Currently this looks for 16 consecutive robots on a 16 alignment. This works for 2 inputs I've tested but I'm not satisfied.
+        if grid.iter().any(|row| (0..101).step_by(0xF).map(|x| 0xFFFF << x).any(|mask| row & mask == mask)) {
+            return i;
+        }
+    }
+    unreachable!()
+}
+
 #[test]
 fn test_part1() {
     assert_eq!(part1(include_str!("../input/day14.txt")), 220_971_520);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(include_str!("../input/day14.txt")), 6355);
 }
