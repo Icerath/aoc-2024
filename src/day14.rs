26a4a46880569b89b6656d0a832a65edccd00fac
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-15 15:18:52 +0200

    day14: I would be so screwed without the discord.

--- src/day14.rs
+++ src/day14.rs
@@ -1,4 +1,3 @@
-#![expect(clippy::cast_sign_loss)]
 use bstr::ByteSlice;
 
 pub fn part1(input: &str) -> u32 {
@@ -37,6 +36,9 @@ pub fn part1(input: &str) -> u32 {
     quadrants.into_iter().product()
 }
 
+const WIDTH: i32 = 101;
+const HEIGHT: i32 = 103;
+
 pub fn part2(input: &str) -> i32 {
     let mut remaining = input.as_bytes();
     let mut robots = vec![];
@@ -57,23 +59,62 @@ pub fn part2(input: &str) -> i32 {
         let nl = remaining.find_byte(b'\n').unwrap();
         let vy: i32 = remaining[..nl].to_str().unwrap().parse().unwrap();
         remaining = &remaining[nl + 1..];
-
         robots.push([px, py, vx, vy]);
     }
-    for i in 0..(101 * 103) {
-        // FIXME: u128s are slow. Use something else.
-        let mut grid = [0u128; 103];
-        for [px, py, vx, vy] in &mut robots {
-            grid[*py as usize] |= 1u128 << *px;
-            *px = (*px + *vx).rem_euclid(101);
-            *py = (*py + *vy).rem_euclid(103);
+
+    let mut x_min_seconds = 0;
+    let mut x_min_value = u32::MAX;
+    for seconds in 0..WIDTH {
+        let mut x_value = 0;
+        for [px, _py, vx, _vy] in &robots {
+            x_value += (px + (vx * seconds)).rem_euclid(WIDTH).abs_diff(WIDTH / 2);
         }
-        // FIXME: Currently this looks for 16 consecutive robots on a 16 alignment. This works for 2 inputs I've tested but I'm not satisfied.
-        if grid.iter().any(|row| (0..101).step_by(0xF).map(|x| 0xFFFF << x).any(|mask| row & mask == mask)) {
-            return i;
+        if x_value < x_min_value {
+            x_min_value = x_value;
+            x_min_seconds = seconds;
         }
     }
-    unreachable!()
+
+    let mut y_min_seconds = 0;
+    let mut y_min_value = u32::MAX;
+
+    for seconds in 0..HEIGHT {
+        let mut y_value = 0;
+        for [_px, py, _vx, vy] in &robots {
+            y_value += (py + (vy * seconds)).rem_euclid(HEIGHT).abs_diff(HEIGHT / 2);
+        }
+        if y_value < y_min_value {
+            y_min_value = y_value;
+            y_min_seconds = seconds;
+        }
+    }
+    big_brain(x_min_seconds, y_min_seconds)
+}
+
+fn big_brain(x: i32, y: i32) -> i32 {
+    const MOD_INV_X: i32 = mod_inv(HEIGHT, WIDTH) * HEIGHT; //5253
+    const MOD_INV_Y: i32 = mod_inv(WIDTH, HEIGHT) * WIDTH; // 5151
+    ((x * MOD_INV_X) + (y * MOD_INV_Y)) % (HEIGHT * WIDTH)
+}
+
+#[expect(clippy::many_single_char_names)]
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+const fn egcd(a: i32, b: i32) -> i32 {
+    let (mut old_r, mut r) = (a, b);
+    let (mut old_s, mut s) = (1, 0);
+    let (mut old_t, mut t) = (0, 1);
+
+    while r != 0 {
+        let quotient = old_r / r;
+        (old_r, r) = (r, old_r - quotient * r);
+        (old_s, s) = (s, old_s - quotient * s);
+        (old_t, t) = (t, old_t - quotient * t);
+    }
+    old_s
+}
+
+const fn mod_inv(x: i32, n: i32) -> i32 {
+    (egcd(x, n) % n + n) % n
 }
 
 #[test]
