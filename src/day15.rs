822921a0f250f848909d53a57e6b074322f8fcdd
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-16 16:34:00 +0200

    day15: initial part2 impl

--- src/day15.rs
+++ src/day15.rs
@@ -53,7 +53,135 @@ unsafe fn part1_inner(input: &[u8]) -> usize {
     result
 }
 
+pub fn part2(input: &str) -> usize {
+    unsafe { part2_inner(input.as_bytes()) }
+}
+
+unsafe fn part2_inner(input: &[u8]) -> usize {
+    let directions = input.get_unchecked(51 * 50 + 2..);
+    let pos = input.find_byte(b'@').unwrap_unchecked();
+    let mut pos = pos % 51 * 2 + pos / 51 * 100;
+
+    let mut grid = [0; 100 * 50];
+
+    for i in 0..50 {
+        for j in 0..50 {
+            let new = match input.get_unchecked(i * 51 + j) {
+                b'.' => b"..",
+                b'#' => b"##",
+                b'O' => b"[]",
+                b'@' => b"@.",
+                _ => unreachable_unchecked(),
+            };
+            grid[i * 100 + j * 2..i * 100 + j * 2 + 2].copy_from_slice(new);
+        }
+    }
+
+    for dir in directions {
+        let dir = match dir {
+            b'^' => -100,
+            b'>' => 1,
+            b'v' => 100,
+            b'<' => -1,
+            _ => continue,
+        };
+
+        let next_pos = pos.wrapping_add_signed(dir);
+        if !can_push_box(&grid, next_pos, dir) {
+            continue;
+        };
+        push_box(&mut grid, next_pos, dir);
+        *grid.get_unchecked_mut(next_pos) = b'@';
+        *grid.get_unchecked_mut(pos) = b'.';
+        pos = next_pos;
+    }
+    let mut result = 0;
+    for i in 1..49 {
+        for j in 1..99 {
+            if *grid.get_unchecked(i * 100 + j) == b'[' {
+                result += 100 * i + j;
+            }
+        }
+    }
+    result
+}
+
 #[test]
 fn test_part1() {
     assert_eq!(part1(include_str!("../input/day15.txt")), 1_412_971);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(include_str!("../input/day15.txt")), 1_429_299);
+}
+
+#[inline(always)]
+unsafe fn can_push_box(grid: &[u8; 100 * 50], pos: usize, dir: isize) -> bool {
+    if dir.abs() == 1 {
+        can_push_box_horizontal(grid, pos, dir)
+    } else {
+        can_push_box_vertical(grid, pos, dir)
+    }
+}
+
+#[inline(always)]
+unsafe fn push_box(grid: &mut [u8; 100 * 50], pos: usize, dir: isize) {
+    if dir.abs() == 1 {
+        push_box_horizontal(grid, pos, dir);
+    } else {
+        push_box_vertical(grid, pos, dir);
+    }
+}
+
+unsafe fn can_push_box_horizontal(grid: &[u8; 100 * 50], mut pos: usize, dir: isize) -> bool {
+    loop {
+        match *grid.get_unchecked(pos) {
+            b'#' => return false,
+            b'.' => return true,
+            b'[' | b']' => pos = pos.wrapping_add_signed(dir),
+            _ => unreachable_unchecked(),
+        }
+    }
+}
+
+unsafe fn can_push_box_vertical(grid: &[u8; 100 * 50], pos: usize, dir: isize) -> bool {
+    match *grid.get_unchecked(pos) {
+        b'#' => false,
+        b'.' => true,
+        b'[' => {
+            can_push_box_vertical(grid, pos.wrapping_add_signed(dir), dir)
+                && can_push_box_vertical(grid, pos.wrapping_add_signed(dir + 1), dir)
+        }
+        b']' => {
+            can_push_box_vertical(grid, pos.wrapping_add_signed(dir), dir)
+                && can_push_box_vertical(grid, pos.wrapping_add_signed(dir - 1), dir)
+        }
+        _ => unreachable_unchecked(),
+    }
+}
+
+unsafe fn push_box_horizontal(grid: &mut [u8; 100 * 50], pos: usize, dir: isize) {
+    if *grid.get_unchecked(pos) == b'.' {
+        return;
+    }
+    let next_pos = pos.wrapping_add_signed(dir);
+    push_box_horizontal(grid, next_pos, dir);
+    *grid.get_unchecked_mut(next_pos) = *grid.get_unchecked(pos);
+    *grid.get_unchecked_mut(pos) = b'.';
+}
+
+unsafe fn push_box_vertical(grid: &mut [u8; 100 * 50], pos: usize, dir: isize) {
+    match *grid.get_unchecked(pos) {
+        b'.' => {}
+        b']' => push_box_vertical(grid, pos - 1, dir),
+        b'[' => {
+            let next_pos = pos.wrapping_add_signed(dir);
+            push_box_vertical(grid, next_pos, dir);
+            push_box_vertical(grid, next_pos + 1, dir);
+            grid.get_unchecked_mut(next_pos..next_pos + 2).copy_from_slice(b"[]");
+            grid.get_unchecked_mut(pos..pos + 2).copy_from_slice(b"..");
+        }
+        _ => unreachable_unchecked(),
+    }
 }
