da72f2423cf2c394403c8881a22152f4b657326e
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-19 15:48:36 +0200

    check results after bench

--- benches/bench_today.rs
+++ benches/bench_today.rs
@@ -5,11 +5,14 @@ use std::hint::black_box;
 fn bench_part1(c: &mut Criterion) {
     let input = include_str!("../input/day18.txt");
     c.bench_function("day18_part1", |b| b.iter(|| part1(black_box(input))));
+
+    assert_eq!(part1(input), 330);
 }
 
 fn bench_part2(c: &mut Criterion) {
     let input = include_str!("../input/day18.txt");
     c.bench_function("day18_part2", |b| b.iter(|| part2(black_box(input))));
+    assert_eq!(part2(input), "10,38");
 }
 
 criterion_group!(benches, bench_part1, bench_part2);
