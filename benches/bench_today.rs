c3c51e2a4b51751b486c328f810355620b4a94a3
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-22 14:02:49 +0200

    fix bench

--- benches/bench_today.rs
+++ benches/bench_today.rs
@@ -5,14 +5,22 @@ macro_rules! bench_day {
         use std::hint::black_box;
 
         fn bench_part1(c: &mut Criterion) {
+            #[inline(never)]
+            fn routine(input: &str) -> impl std::fmt::Display {
+                part2(black_box(input))
+            }
             let input = include_str!(concat!("../input/", stringify!($day), ".txt"));
-            c.bench_function(concat!(stringify!($day), " part1"), |b| b.iter(|| part1(black_box(input))));
+            c.bench_function(concat!(stringify!($day), " part1"), |b| b.iter(|| routine(input)));
             assert_eq!(part1(input), PART1_OUT);
         }
 
         fn bench_part2(c: &mut Criterion) {
+            #[inline(never)]
+            fn routine(input: &str) -> impl std::fmt::Display {
+                part2(black_box(input))
+            }
             let input = include_str!(concat!("../input/", stringify!($day), ".txt"));
-            c.bench_function(concat!(stringify!($day), " part2"), |b| b.iter(|| part2(black_box(input))));
+            c.bench_function(concat!(stringify!($day), " part2"), |b| b.iter(|| routine(input)));
             assert_eq!(part2(input), PART2_OUT);
         }
 
