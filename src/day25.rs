d2b845d18bcb7b93e22c10e00ad39689a175e702
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-12-26 11:12:02 +0200

    use static muts

--- src/day25.rs
+++ src/day25.rs
@@ -1,15 +1,25 @@
+#![expect(static_mut_refs)]
+
 pub fn part1(input: &str) -> u32 {
-    let mut input = input.as_bytes();
-    let mut locks = vec![];
-    let mut keys = vec![];
+    unsafe { part1_inner(input.as_bytes()) }
+}
+unsafe fn part1_inner(mut input: &[u8]) -> u32 {
+    static mut LOCKS: [u32; 250] = [0; 250];
+    static mut KEYS: [u32; 250] = [0; 250];
+
+    let mut num_locks = 0;
+    let mut num_keys = 0;
     loop {
         let mut bits = 0;
         for i in 0..30 {
             bits |= ((input[i + 6] == b'#') as u32) << i;
         }
-        match input[0] {
-            b'#' => locks.push(bits),
-            _ => keys.push(bits),
+        if input[0] == b'#' {
+            LOCKS[num_locks] = bits;
+            num_locks += 1;
+        } else {
+            KEYS[num_keys] = bits;
+            num_keys += 1;
         }
         input = match input.get(43..) {
             Some(input) => input,
@@ -17,8 +27,8 @@ pub fn part1(input: &str) -> u32 {
         };
     }
     let mut num_matching = 0;
-    for lock in &locks {
-        for key in &keys {
+    for lock in &LOCKS {
+        for key in &KEYS {
             num_matching += (lock & key == 0) as u32;
         }
     }
